315e4f3705670d3fcceca2c1efa767c7fff98da5
Shubh <202152323@iiitvadodara.ac.in>
2022-11-15 09:54:09 +0530

    password

--- app/src/main/java/com/example/placementcellapp/signup.java
+++ app/src/main/java/com/example/placementcellapp/signup.java
@@ -1,6 +1,10 @@
 package com.example.placementcellapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class signup extends AppCompatActivity{
@@ -9,6 +13,18 @@ public class signup extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
 
+        Button button = (Button) findViewById(R.id.signup);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openDashboard();
+            }
+        });
+    }
+
+    public void openDashboard(){
+        Intent intent = new Intent(this, dashboard.class);
+        startActivity(intent);
         getSupportActionBar().hide();
     }
 }
