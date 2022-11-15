be0860ee8c22d7200b7b80969eace4b503cdae8b
Shubh <202152323@iiitvadodara.ac.in>
2022-11-15 10:48:59 +0530

    password visbible

--- app/src/main/java/com/example/placementcellapp/signup.java
+++ app/src/main/java/com/example/placementcellapp/signup.java
@@ -1,30 +1,75 @@
 package com.example.placementcellapp;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import static com.example.placementcellapp.R.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class signup extends AppCompatActivity{
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class signup extends AppCompatActivity {
+
+    EditText student_id, password;
+    boolean passwordVisible;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signup);
+        setContentView(layout.signup);
 
-        Button button = (Button) findViewById(R.id.signup);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+        Button button = findViewById(R.id.signup);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 openDashboard();
             }
         });
+
+        student_id = findViewById(id.studentID);
+        password = findViewById(id.Password);
+
+        password.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                final int Right = 2;
+
+                if(motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    if(motionEvent.getRawX() >= password.getRight()-password.getCompoundDrawables()[Right].getBounds().width()){
+                        int selection = password.getSelectionEnd();
+                        if(passwordVisible){
+                            // set drawable image here
+                            password.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0, drawable.password_invisible,0);
+                            // for hide password
+                            password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                            passwordVisible = false;
+                        }
+                        else{
+                            password.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0, drawable.password_visible,0);
+                            // for show password
+                            password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                            passwordVisible = true;
+                        }
+                        password.setSelection(selection);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
     }
 
     public void openDashboard(){
         Intent intent = new Intent(this, dashboard.class);
         startActivity(intent);
-        getSupportActionBar().hide();
     }
+
 }
