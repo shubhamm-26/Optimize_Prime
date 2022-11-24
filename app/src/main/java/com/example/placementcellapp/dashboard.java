73c5a75ef73c090daf7bbb03dd4ec67b50807569
Shubh <202152323@iiitvadodara.ac.in>
2022-11-24 11:32:34 +0530

    tablayout

--- app/src/main/java/com/example/placementcellapp/dashboard.java
+++ app/src/main/java/com/example/placementcellapp/dashboard.java
@@ -6,19 +6,57 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class dashboard extends Fragment
 {
+        TabLayout tabLayout;
+        ViewPager2 viewPager2;
+
+        tab_adapter adapter;
+
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
                 ViewGroup root = (ViewGroup) inflater.inflate(R.layout.dashboard,container,false);
+                tabLayout = root.findViewById(R.id.tablayout);
+                viewPager2 = root.findViewById(R.id.view_pager2);
+                tabLayout.addTab(tabLayout.newTab().setText("Events"));
+                tabLayout.addTab(tabLayout.newTab().setText("News"));
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                adapter = new tab_adapter(fragmentManager,getLifecycle());
+                viewPager2.setAdapter(adapter);
+                tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+                        @Override
+                        public void onTabSelected(TabLayout.Tab tab) {
+                                viewPager2.setCurrentItem(tab.getPosition());
+                        }
+
+                        @Override
+                        public void onTabUnselected(TabLayout.Tab tab) {
+
+                        }
+
+                        @Override
+                        public void onTabReselected(TabLayout.Tab tab) {
+
+                        }
+                });
+                viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+                        @Override
+                        public void onPageSelected(int position) {
+                                tabLayout.selectTab(tabLayout.getTabAt(position));
+                        }
+                });
+
         //Image Slider
         ImageSlider imageSlider = root.findViewById(R.id.image_slider);
         List<SlideModel> slideModels = new ArrayList<>();
---
+++ app/src/main/java/com/example/placementcellapp/tab_adapter.java
@@ -0,0 +1,37 @@
+package com.example.placementcellapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+
+
+public class tab_adapter extends FragmentStateAdapter
+{
+    private final String[] fragmenttitle = new String[]{"events", "news"};
+
+    public tab_adapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        if(position==1)
+        {
+            return new tab_events();
+        }
+        else return new tab_news();
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
+
