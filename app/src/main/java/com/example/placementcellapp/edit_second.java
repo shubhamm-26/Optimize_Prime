4f1aed950772348d96e049a28cced64ae80be7a5
Shubh <202152323@iiitvadodara.ac.in>
2022-12-07 15:27:15 +0530

    profile

---
+++ app/src/main/java/com/example/placementcellapp/edit_second.java
@@ -0,0 +1,14 @@
+package com.example.placementcellapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class edit_second extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.editprofile_second);
+    }
+}
--- app/src/main/java/com/example/placementcellapp/userprofile.java
+++ app/src/main/java/com/example/placementcellapp/userprofile.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +17,7 @@ import androidx.fragment.app.Fragment;
 public class userprofile extends Fragment {
     @Nullable
     Button Logout;
+    TextView edit;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.userprofile,container,false);
         Logout = root.findViewById(R.id.Logout);
@@ -25,15 +27,28 @@ public class userprofile extends Fragment {
                 openlogin();
             }
         });
+
+        edit = root.findViewById(R.id.edit);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editprof_first();
+            }
+        });
         return root;
 
     }
+
 
     private void openlogin()
     {
         Intent intent = new Intent(getActivity(),Login.class);
         startActivity(intent);
-
+    }
+    private void editprof_first()
+    {
+        Intent intent = new Intent(getActivity(),edit_first.class);
+        startActivity(intent);
     }
 
 }
